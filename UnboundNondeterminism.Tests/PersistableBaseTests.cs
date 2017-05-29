2eab9d4c3441936a3935f8bf18f4d8132d4959cd
jameswilddev <jameswilddev@outlook.com>
2017-05-29 10:27:41 +0100

    Added a PersistableBase class which handles some of the boilerplate associated with creating persistable actors.

---
+++ UnboundNondeterminism.Tests/PersistableBaseTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Akka.TestKit.Xunit2;
+using Xunit;
+using Akka.Actor;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnboundedNondeterminism.Tests
+{
+    public sealed class PersistableBaseTests : TestKit
+    {
+        public static SynchronizedCollection<Guid> Created = new SynchronizedCollection<Guid>();
+        public static SynchronizedCollection<Guid> Deleted = new SynchronizedCollection<Guid>();
+        public static SynchronizedCollection<object> Sent = new SynchronizedCollection<object>();
+
+        protected override void AfterAll()
+        {
+            // Stop the actors before clearing the lists they write to.
+            base.AfterAll();
+            Created.Clear();
+            Deleted.Clear();
+            Sent.Clear();
+        }
+
+        public sealed class TestType : PersistableBase
+        {
+            protected override void PostStop()
+            {
+                Deleted.Add(PersistenceGuid);
+                base.PostStop();
+            }
+
+            public TestType(Guid persistenceGuid) : base(persistenceGuid)
+            {
+                Created.Add(persistenceGuid);
+                Command(Sent.Add);
+            }
+        }
+
+        [Fact]
+        public void DoesNothingWithoutBeingSentMessages()
+        {
+            var guid = Guid.NewGuid();
+            var instance = Sys.ActorOf(Props.Create(() => new TestType(guid)));
+
+            ExpectNoMsg();
+            Assert.Equal(new[] { guid }, Created);
+            Assert.Empty(Deleted);
+            Assert.Empty(Sent);
+        }
+
+        [Fact]
+        public void StopsWhenSentStop()
+        {
+            var guid = Guid.NewGuid();
+            var instance = Sys.ActorOf(Props.Create(() => new TestType(guid)));
+
+            instance.Tell(new PersistableBase.Stop());
+
+            ExpectNoMsg();
+            Assert.Equal(new[] { guid }, Created);
+            Assert.Equal(new[] { guid }, Deleted);
+            Assert.Empty(Sent);
+        }
+
+        [Fact]
+        public void DoesNotInterceptOtherMessages()
+        {
+            var guid = Guid.NewGuid();
+            var instance = Sys.ActorOf(Props.Create(() => new TestType(guid)));
+
+            instance.Tell("Test message one");
+            ExpectNoMsg();
+            instance.Tell("Test message two");
+            ExpectNoMsg();
+
+            Assert.Equal(new[] { guid }, Created);
+            Assert.Empty(Deleted);
+            Assert.Equal(new[] { "Test message one", "Test message two" }, Sent);
+        }
+    }
+}
---
+++ UnboundedNondeterminism/PersistableBase.cs
@@ -0,0 +1,41 @@
+﻿using Akka.Actor;
+using Akka.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnboundedNondeterminism
+{
+    /// <summary>
+    /// A base class which extends <see cref="ReceivePersistentActor"/> to:
+    /// • Use a <see cref="Eventsourced.PersistenceId"/> generated from the <see cref="MemberInfo.Name"/> and a <see cref="Guid"/>.
+    /// • Handle a <see cref="PersistableBase.Stop"/> message to safely stop the actor.
+    /// </summary>
+    public abstract class PersistableBase : ReceivePersistentActor
+    {
+        /// <summary>Instructs an <see cref="ActorBase"/> to stop when possible.</summary>
+        /// <remarks>http://getakka.net/docs/persistence/persistent-actors#safely-shutting-down-persistent-actors</remarks>
+        public sealed class Stop { }
+
+        /// <summary>Returned in response to <see cref="Stop"/> when the <see cref="ActorBase"/> has stopped.</summary>
+        public sealed class Stopped { }
+
+        /// <summary>A <see cref="Guid"/> for <see langword="this"/>.</summary>
+        public readonly Guid PersistenceGuid;
+
+        /// <inheritdoc />
+        public override string PersistenceId => $"{GetType().Name}-{PersistenceGuid}";
+
+        /// <summary>Call from extending classes to </summary>
+        /// <param name="persistenceGuid"><see cref="PersistenceGuid"/>.</param>
+        public PersistableBase(Guid persistenceGuid)
+        {
+            PersistenceGuid = persistenceGuid;
+
+            Command<Stop>(s => Context.Stop(Self));
+        }
+    }
+}
