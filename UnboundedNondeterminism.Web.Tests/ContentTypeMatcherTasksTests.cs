de7d00269bea9e1e988c6e87c0eaa360b49302dd
jameswilddev <jameswilddev@outlook.com>
2017-05-29 23:57:06 +0100

    Implemented an actor which parses and sorts sets of content types.

---
+++ UnboundedNondeterminism.Web.Tests/ContentTypeMatcherTasksTests.cs
@@ -0,0 +1,104 @@
+﻿using Akka.Actor;
+using Akka.TestKit.Xunit2;
+using System;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using Xunit;
+
+namespace UnboundedNondeterminism.Web.Tests
+{
+    public sealed class ContentTypeMatcherTasksTests : TestKit
+    {
+        #region ParseAndSort
+        public static Encoding RequestDefault = new DummyEncoding();
+        public static ContentTypeParser.Parsed ParsedA = new ContentTypeParser.Parsed { Priority = 6.0m };
+        public static ContentTypeParser.Parsed ParsedB = new ContentTypeParser.Parsed { Priority = 6.4m };
+        public static ContentTypeParser.Parsed ParsedC = new ContentTypeParser.Parsed { Priority = 4.0m };
+        public static ContentTypeParser.Parsed ParsedD = new ContentTypeParser.Parsed { Priority = 3.0m };
+        public static ContentTypeParser.Parsed ParsedE = new ContentTypeParser.Parsed { Priority = 2.7m };
+        public static ContentTypeParser.Parsed ParsedF = new ContentTypeParser.Parsed { Priority = 9.4m };
+        public static ContentTypeParser.Parsed ParsedG = new ContentTypeParser.Parsed { Priority = 4.4m };
+        public static ContentTypeParser.Parsed ParsedH = new ContentTypeParser.Parsed { Priority = 6.5m };
+
+        public sealed class DummyContentTypeParser : ReceiveActor
+        {
+            public DummyContentTypeParser()
+            {
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "test unparseable a" && p.RequestDefault == RequestDefault, p => Sender.Tell(new ContentTypeParser.Unparseable()));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "test unparseable b" && p.RequestDefault == RequestDefault, p => Sender.Tell(new ContentTypeParser.Unparseable()));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "test unparseable c" && p.RequestDefault == RequestDefault, p => Sender.Tell(new ContentTypeParser.Unparseable()));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "test unparseable d" && p.RequestDefault == RequestDefault, p => Sender.Tell(new ContentTypeParser.Unparseable()));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "test parseable a" && p.RequestDefault == RequestDefault, p => Sender.Tell(ParsedA));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "test parseable b" && p.RequestDefault == RequestDefault, p => Sender.Tell(ParsedB));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "test parseable c" && p.RequestDefault == RequestDefault, p => Sender.Tell(ParsedC));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "test parseable d" && p.RequestDefault == RequestDefault, p => Sender.Tell(ParsedD));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "alternative test parseable a" && p.RequestDefault == RequestDefault, p => Sender.Tell(ParsedE));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "alternative test parseable b" && p.RequestDefault == RequestDefault, p => Sender.Tell(ParsedF));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "alternative test parseable c" && p.RequestDefault == RequestDefault, p => Sender.Tell(ParsedG));
+                Receive<ContentTypeParser.Parse>(p => p.ContentType == "alternative test parseable d" && p.RequestDefault == RequestDefault, p => Sender.Tell(ParsedH));
+                ReceiveAny(m => Assert.True(false));
+            }
+        }
+
+        [Fact]
+        public void ParseAndSortNothingReturnsEmptyParsedAndSorted()
+        {
+            var contentTypeParser = Sys.ActorOf(Props.Create(() => new DummyContentTypeParser()));
+            var tasks = Sys.ActorOf(Props.Create(() => new ContentTypeMatcherTasks(contentTypeParser)));
+
+            tasks.Tell(new ContentTypeMatcherTasks.ParseAndSort { ContentTypes = Enumerable.Empty<string>(), RequestDefault = RequestDefault });
+
+            Assert.Empty(ExpectMsgFrom<ContentTypeMatcherTasks.ParsedAndSorted>(tasks).ContentTypes);
+        }
+
+        [Fact]
+        public void ParseAndSortNothingParsesReturnsEmptyParsedAndSorted()
+        {
+            var contentTypeParser = Sys.ActorOf(Props.Create(() => new DummyContentTypeParser()));
+            var tasks = Sys.ActorOf(Props.Create(() => new ContentTypeMatcherTasks(contentTypeParser)));
+
+            tasks.Tell(new ContentTypeMatcherTasks.ParseAndSort { ContentTypes = new[] { "test unparseable a", "test unparseable b", "test unparseable c", "test unparseable d" }, RequestDefault = RequestDefault });
+
+            Assert.Empty(ExpectMsgFrom<ContentTypeMatcherTasks.ParsedAndSorted>(tasks).ContentTypes);
+        }
+
+        [Fact]
+        public void ParseAndSortSomeParseReturnsOnlyParsedSorted()
+        {
+            var contentTypeParser = Sys.ActorOf(Props.Create(() => new DummyContentTypeParser()));
+            var tasks = Sys.ActorOf(Props.Create(() => new ContentTypeMatcherTasks(contentTypeParser)));
+
+            tasks.Tell(new ContentTypeMatcherTasks.ParseAndSort { ContentTypes = new[] { "test parseable a", "test parseable b", "test unparseable c", "test parseable d" }, RequestDefault = RequestDefault });
+
+            Assert.Equal(new[] { ParsedB, ParsedA, ParsedD }, ExpectMsgFrom<ContentTypeMatcherTasks.ParsedAndSorted>(tasks).ContentTypes);
+        }
+
+        [Fact]
+        public void ParseAndSortAllParseReturnsParsedSorted()
+        {
+            var contentTypeParser = Sys.ActorOf(Props.Create(() => new DummyContentTypeParser()));
+            var tasks = Sys.ActorOf(Props.Create(() => new ContentTypeMatcherTasks(contentTypeParser)));
+
+            tasks.Tell(new ContentTypeMatcherTasks.ParseAndSort { ContentTypes = new[] { "test parseable a", "test parseable b", "test parseable c", "test parseable d" }, RequestDefault = RequestDefault });
+
+            Assert.Equal(new[] { ParsedB, ParsedA, ParsedC, ParsedD }, ExpectMsgFrom<ContentTypeMatcherTasks.ParsedAndSorted>(tasks).ContentTypes);
+        }
+
+        [Fact]
+        public void ParseAndSortIsStateless()
+        {
+            var contentTypeParser = Sys.ActorOf(Props.Create(() => new DummyContentTypeParser()));
+            var tasks = Sys.ActorOf(Props.Create(() => new ContentTypeMatcherTasks(contentTypeParser)));
+            var probeA = CreateTestProbe();
+            var probeB = CreateTestProbe();
+
+            tasks.Tell(new ContentTypeMatcherTasks.ParseAndSort { ContentTypes = new[] { "test parseable a", "test parseable b", "test parseable c", "test parseable d" }, RequestDefault = RequestDefault }, probeA);
+            tasks.Tell(new ContentTypeMatcherTasks.ParseAndSort { ContentTypes = new[] { "alternative test parseable a", "alternative test parseable b", "alternative test parseable c", "alternative test parseable d" }, RequestDefault = RequestDefault }, probeB);
+
+            Assert.Equal(new[] { ParsedB, ParsedA, ParsedC, ParsedD }, probeA.ExpectMsgFrom<ContentTypeMatcherTasks.ParsedAndSorted>(tasks).ContentTypes);
+            Assert.Equal(new[] { ParsedF, ParsedH, ParsedG, ParsedE }, probeB.ExpectMsgFrom<ContentTypeMatcherTasks.ParsedAndSorted>(tasks).ContentTypes);
+        }
+        #endregion
+    }
+}
---
+++ UnboundedNondeterminism.Web/ContentTypeMatcherTasks.cs
@@ -0,0 +1,72 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Web;
+
+namespace UnboundedNondeterminism.Web
+{
+    /// <summary>Implements tasks which need to be performed to match <see cref="ContentType"/>s between <see cref="HttpRequest.AcceptTypes"/> and those we support.</summary>
+    public sealed class ContentTypeMatcherTasks : ReceiveActor
+    {
+        /// <summary>A request to parse a set of <see cref="string"/>s into <see cref="ContentType"/>s, and sort them by <see cref="ContentTypeParser.Parsed.Priority"/> descending.</summary>
+        public sealed class ParseAndSort
+        {
+            /// <summary>The <see cref="ContentType"/>s received in <see cref="HttpRequest.AcceptTypes"/>.</summary>
+            public IEnumerable<string> ContentTypes;
+
+            /// <summary>The default <see cref="Encoding"/> to use if one cannot be found in <see cref="ContentType"/>s.</summary>
+            public Encoding RequestDefault;
+        }
+
+        /// <summary>Returned in response to <see cref="ParseAndSort"/>.</summary>
+        public sealed class ParsedAndSorted
+        {
+            /// <summary>The parsed <see cref="ContentType"/>s, if any, sorted by <see cref="ContentTypeParser.Parsed.Priority"/> descending.</summary>
+            public IEnumerable<ContentTypeParser.Parsed> ContentTypes;
+        }
+
+        /// <inheritdoc />
+        /// <param name="contentTypeParser">The <see cref="ContentTypeParser"/> to defer to.</param>
+        public ContentTypeMatcherTasks(IActorRef contentTypeParser)
+        {
+            Receive<ParseAndSort>(pas => !pas.ContentTypes.Any(), pas => Sender.Tell(new ParsedAndSorted { ContentTypes = Enumerable.Empty<ContentTypeParser.Parsed>() }));
+            Receive<ParseAndSort>(pas => Context.ActorOf(Props.Create(() => new ParseAndSortAggregator(contentTypeParser, Self, Sender, pas.ContentTypes, pas.RequestDefault))));
+        }
+
+        /// <summary>Aggregates responses from <see cref="ContentTypeParser"/> to generate a completed <see cref="ParsedAndSorted"/>.</summary>
+        /// <remarks>One instance is created per <see cref="ParseAndSort"/>.</remarks>
+        public sealed class ParseAndSortAggregator : ReceiveActor
+        {
+            /// <inheritdoc />
+            /// <param name="contentTypeParser">The <see cref="ContentTypeParser"/> to defer to.</param>
+            /// <param name="sender">The <see cref="ContentTypeMatcherTasks"/>.</param>
+            /// <param name="recipient">The <see cref="IActorRef"/> to send the resulting <see cref="ParsedAndSorted"/> to.</param>
+            /// <param name="contentTypes">The <see cref="string"/>s to parse to <see cref="ContentType"/>s.</param>
+            /// <param name="requestDefault">The default <see cref="Encoding"/> to use if one cannot be found in the parsed <see cref="ContentType"/>.</param>
+            public ParseAndSortAggregator(IActorRef contentTypeParser, IActorRef sender, IActorRef recipient, IEnumerable<string> contentTypes, Encoding requestDefault)
+            {
+                var remaining = contentTypes.Count();
+                var parsed = new List<ContentTypeParser.Parsed>();
+
+                foreach (var contentType in contentTypes) contentTypeParser.Tell(new ContentTypeParser.Parse { ContentType = contentType, RequestDefault = requestDefault });
+
+                Action checkDone = () =>
+                {
+                    remaining--;
+                    if (remaining == 0) recipient.Tell(new ParsedAndSorted { ContentTypes = parsed.OrderByDescending(p => p.Priority) }, sender);
+                };
+
+                Receive<ContentTypeParser.Parsed>(p =>
+                {
+                    parsed.Add(p);
+                    checkDone();
+                });
+
+                Receive<ContentTypeParser.Unparseable>(u => checkDone());
+            }
+        }
+    }
+}
